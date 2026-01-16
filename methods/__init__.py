b8e620bcb0fab27efba03302da1cb5b837485860
Christophe Muller <mullerchristophe21@gmail.com>
2026-01-16 09:50:29 +0000

    Refactor: Remove investigate_weights.py and reorganize EnergyAugmenter classes for clarity

--- methods/__init__.py
+++ methods/__init__.py
@@ -1,7 +1,7 @@
-from .methods_weighted import EnergyAugmenter_Matching
-from .methods_pooledTarget import EnergyAugmenter_PooledTarget
-from .methods_weighted_reg import EnergyAugmenter_MatchingReg
+from .methods_EnergyMatching import EnergyAugmenter_Matching
+from .methods_EnergyMatchingPooledTarget import EnergyAugmenter_PooledTarget
+from .methods_EnergyMatchingReg import EnergyAugmenter_MatchingReg
 from .methods_ipw import IPWAugmenter
-from .methods_weights import EnergyAugmenter_Weighting
+from .methods_EnergyWeighting import EnergyAugmenter_Weighting
 
 __all__ = ["EnergyAugmenter_Matching", "EnergyAugmenter_PooledTarget", "EnergyAugmenter_MatchingReg", "IPWAugmenter", "EnergyAugmenter_Weighting"]
